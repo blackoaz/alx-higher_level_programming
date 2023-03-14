bd9f5a4d070824d889eabdb48041cd5ea6febe42
blackoaz <pauloakello56@gmail.com>
2023-03-14 13:12:35 -0700

    function for checking index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if 0 > idx > len(my_list):
+    if 0 > idx >= len(my_list):
             return none
-        else:
-            return my_list(idx)
+    else:
+        return my_list(idx)
