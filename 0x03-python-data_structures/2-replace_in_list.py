d07258329a23426ef3608e9c1a0dda6634740335
blackoaz <pauloakello56@gmail.com>
2023-03-14 13:43:43 -0700

    adding function for replacing ana index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for idx in my_list:
-        if my_list[idx] < 0 or my_list[idx] > len(my_list):
+        if my_list[idx] < 0 or my_list[idx] >= len(my_list):
             return my_list
         else:
             my_list[idx] = element
+            return my_list
