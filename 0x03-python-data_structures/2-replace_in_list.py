b65c236484bf74ee45a3a6697962b2954c33254f
blackoaz <pauloakello56@gmail.com>
2023-03-14 13:30:32 -0700

    replace index

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for idx in my_list:
+        if my_list[idx] < 0 or my_list[idx] > len(my_list):
+            return my_list
+        else:
+            my_list[idx] = element
