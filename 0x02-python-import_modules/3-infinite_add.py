6016cda83cb0dd0614f4e26b1de58197149f5ec4
blackoaz <pauloakello56@gmail.com>
2023-03-15 07:13:03 -0700

    summing up arguments value

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == '__main__':
-    sum = 0;
+    sum = 0
     for i in range(len(argv)-1):
         if len(argv) == 1:
             print(0)
         else:
-            sum+=int(argv[i+1])
-    print(sum)        
+            sum + =int(argv[i+1])
+    print(sum)
