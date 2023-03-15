9144205b873b0c80c5266cc1b6a9c9a4259d1942
blackoaz <pauloakello56@gmail.com>
2023-03-15 14:03:48 -0700

    function for calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,12 +4,10 @@ from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     if len(argv) != 4:
         print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     operator = argv[2]
-
-
 
     if operator == '+':
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
