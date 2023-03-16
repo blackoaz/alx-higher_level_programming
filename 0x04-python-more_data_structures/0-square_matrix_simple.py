b610066529814db535e81bd6569b55bf6a8756c8
blackoaz <pauloakello56@gmail.com>
2023-03-16 11:14:27 -0700

    updates on matrix square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
-    for row in new_matrix:
-        for element in row:
-            print(element ** 2, end=" " if element != row[-1] else "")
-        print()
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([])
+        for j in matrix[i]:
+            new_matrix.append(j**2)
+        return(new_matrix)
