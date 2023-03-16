3920a92d0db9140febd21f30e4a5280e1771f67b
blackoaz <pauloakello56@gmail.com>
2023-03-16 14:01:56 -0700

    adding unique values in a list

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,5 @@
-#!/usr/nin/python3
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_elements = set(set_1) - (set(set_1) - set(set_2))
-    return (common_elements)
+    a = set_1
+    b = set_2
+    return a & b
