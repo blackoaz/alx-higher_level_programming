15014eb0571aa35472b7e3928cc5129a20c40481
blackoaz <pauloakello56@gmail.com>
2023-03-16 11:43:37 -0700

    find and reolace in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)-1):
-        if my_list[i] == search:
-            my_list[i] = replace        
-my_list = [1,2,3,4,5,6] 
-print(my_list)
-search_replace(my_list, 3, 10)
-print(my_list)
+    new_list = my_list.copy()
+    for i in my_list:
+        if i is search:
+            new_list[i] = replace
+    return (new_list)
