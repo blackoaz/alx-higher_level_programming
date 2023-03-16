cc8eedac227c89c0a38fc29be788e2495df490b7
blackoaz <pauloakello56@gmail.com>
2023-03-16 13:39:42 -0700

    adding unique values in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def unq_add(my_list=[]):
-    if my_list is None:
-        my_list = []
-    new_list = []
-    x = 0
-    for i in my_list:
-        if i not in new_list:
-            new_list.append(i)
-            x += i
-    return x
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    unique_list = []
+    for x in my_list:
+        if x not in unique_list:
+            unique_list.append(x)
+    for x in unique_list:
+        sum += x
+    return (sum)
