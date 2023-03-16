62eb1f57a75e54e945804c321c4d2ecfb9752a21
blackoaz <pauloakello56@gmail.com>
2023-03-16 11:06:30 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for row in matrix:
+    new_matrix = matrix
+    for row in new_matrix:
         for element in row:
             print(element ** 2, end=" " if element != row[-1] else "")
         print()
