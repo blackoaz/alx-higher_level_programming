6799392f988e75b4a7255d196410e80b6d78940c
blackoaz <pauloakello56@gmail.com>
2023-03-28 23:42:36 -0700

    changes to task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    len_list = 0
-    for item in my_list:
-        len_list = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
@@ -11,11 +8,3 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         print('The given value for x is out of range')
         return 0
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
