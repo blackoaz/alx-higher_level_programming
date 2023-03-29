79fb0711eba9e31bb3e40d8c99843d191d5f44dd
blackoaz <pauloakello56@gmail.com>
2023-03-29 09:33:13 -0700

    initializing the square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""a square class with private instance attribute size"""
+
+
+class Square:
+    """This is a square class"""
+
+
+    def __init__(self, size):
+        """Initializing a new square """
+        self.__size = size
