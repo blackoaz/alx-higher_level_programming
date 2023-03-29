6fdcae861e5d52e5a3d7d88c35a78c460e87a93c
blackoaz <pauloakello56@gmail.com>
2023-03-29 09:17:32 -0700

    adding task 0 for creating a square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function for creating an empty class for square that defines a square
+"""
+
+
+class Square:
+    pass
+
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
