5b9647185d62832a79aaf2914c351672abb33084
blackoaz <pauloakello56@gmail.com>
2023-03-29 02:56:58 -0700

    adding task 4 for exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,6 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         for j in range(list_length):
+            result = 0
             try:
                 result = my_list_1[i] / my_list_2[j]
             except TypeError:
