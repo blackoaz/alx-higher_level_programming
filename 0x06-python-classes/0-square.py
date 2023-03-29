72961442e8c981fbde8d22008b63a10a299a645f
blackoaz <pauloakello56@gmail.com>
2023-03-29 09:23:19 -0700

    updates to square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -7,8 +7,3 @@ function for creating an empty class for square that defines a square
 class Square:
     """"This is a python class"""
     pass
-
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
