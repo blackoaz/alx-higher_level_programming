11b61b3649db9d4eb31df2a088e769777f469e93
blackoaz <pauloakello56@gmail.com>
2023-03-29 01:47:45 -0700

    adding updates to task 2 of exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,6 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num += 1
+        except IndexError:
+            break
         except Exception:
             continue
     print()
