71abfc1e5a503c4f216e7b6768a8041ab01c48cd
blackoaz <pauloakello56@gmail.com>
2023-04-12 06:58:31 -0700

    function for checking attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function for returning the list of available attributes and methods"""
+
+
+def lookup(obj):
+    """function for returning attributes"""
+
+    return dir(obj)
