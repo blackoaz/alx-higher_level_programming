4af7eae40d3979736c68ba49724c13b37386aca7
blackoaz <pauloakello56@gmail.com>
2023-04-12 23:44:34 -0700

    adding area method to Rectangle class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+base geometry class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from basegeometry"""
+
+    def __init__(self, width, height):
+        """constructor method"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = heighti
+
+    def area(self):
+        """function for calculating area of a rectangle"""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """string function"""
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
