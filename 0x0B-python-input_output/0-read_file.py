be135334e729182f8ebdc6e96f72f72dbccbd379
blackoaz <pauloakello56@gmail.com>
2023-04-13 09:41:08 -0700

    updates to task 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """function for opening file for reading"""
 
-    with open('filename', 'r', encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
