42a68574d5fa7915d0e1675f7a7d54c6a31bcd3f
blackoaz <pauloakello56@gmail.com>
2023-04-13 11:28:39 -0700

    updates to task 11

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""module for creating a student file"""
+"""Module for creating a student file
+"""
 
 
 class Student:
-    """class for student"""
+    """Class that defines a student
+    """
 
     def __init__(self, first_name, last_name, age):
-        """constructor for class student"""
+        """constructor for student instance"""
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """method for retrieving a dictionary"""
+    def to_json(self, attrs=None):
+        """Retrieving a dictionary representation of a student instance
+        """
 
         my_dict = dict()
-        if type(attrbs) is list and all(type(x) is str for x in attrs):
+        if type(attrs) is list and all(type(x) is str for x in attrs):
             for x in attrs:
                 if x in self.__dict__:
-                    my_dict.update({x: self.__dict__[x]})
+                    my_dict.upate({x: self.__dict__[x]})
             return my_dict
-
         return self.__dict__.copy()
