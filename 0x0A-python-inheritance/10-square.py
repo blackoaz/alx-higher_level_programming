862c20e177b06fcdd82efc8a8dc797c6e2f5f570
blackoaz <pauloakello56@gmail.com>
2023-04-13 00:04:20 -0700

    adding task 10, square method, inheritting from the Rectangle class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""square class that inherits from the class rectangle"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """"square class inheriting from rectangle"""
+
+    def __init__(self, size):
+        """initialization for square class"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+        def __str__(self):
+            return super.__str__()
+
+        def area(self):
+            """calculates area method for square, therefore
+            overwritting the area method for rectangle
+            """
+
+            return self.__size ** 2
