e8a045e634f41f44282699cba0630244874bb83c
blackoaz <pauloakello56@gmail.com>
2023-04-13 11:34:02 -0700

    adding task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module for creating a student file
+"""
+
+
+class Student:
+    """Class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """constructor for student instance"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        json_dict = self.__dict__
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            json_dict = {}
+            try:
+                for k, v in self.__dict__.items():
+                    if k in attrs:
+                        json_dict[k] = v
+            except KeyError:
+                pass
+        return json_dict
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
