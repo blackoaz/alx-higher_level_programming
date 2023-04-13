dfc8d4536ce4dfb5f7b1af20e936494a1cd239ac
blackoaz <pauloakello56@gmail.com>
2023-04-13 09:38:16 -0700

    function for reading file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that read text files"""
+
+
+def read_file(filename=""):
+    """function for opening file for reading"""
+
+    with open('filename', 'r', encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
