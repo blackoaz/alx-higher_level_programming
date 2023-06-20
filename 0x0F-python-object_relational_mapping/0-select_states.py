0bc5df2c2a0dfda9de0b93f83a3c42e17c602ce9
blackoaz <pauloakello56@gmail.com>
2023-06-20 12:36:52 -0700

    adding task 1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script for listing all states in a database in a ascending order"""
+
+
+import MySQLdb
+from sys import argv
+
+
+def get_states(username, password, database):
+    """function for getting all the states"""
+    db = MySQLdb.cnnect(host="localhost", port="3306", password=str(password),
+                        user=str(username), database=str(database),
+                        charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    states = curs.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
