69eecfa8a6b5841ff2e88ffe6613f4129f16412e
blackoaz <pauloakello56@gmail.com>
2023-06-21 14:48:22 -0700

    updates

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,32 +5,13 @@ import sys
 import MySQLdb
 
 
-def select_city_by_state(username, password, database, state_name):
-    """Selecting cities based on states"""
 
-    db = MySQLdb.connect(host="localhost", user=str(username), port=3306,
-                         password=str(password), database=str(databse))
-    cur = db.cursor()
-    cur.execute("SELECT * FROM cities WHERE state = %s ORDER BY id ASC",
-                (state_name))
-    cities = cur.fetchall()
-    for city in cities:
-        print(city)
-    db.close()
-    cur.close()
-
-
-if __name__ == '__main__':
-    # Check if all 4 arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: python script.py < username > < password > < database >
-              < state_name >")
-    else:
-        # Retrieve the command-line arguments
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
-
-        # Call the function to list cities by state
-        select_city_by_state(username, password, database, state_name)
+if __name__ == "__main__":
+    """script should take 4 arguments"""
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                 INNER JOIN `states` as `s` \
+                    ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
