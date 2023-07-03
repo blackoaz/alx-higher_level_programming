ecf2b4da603ed97b4f03d7322644906be086c5be
blackoaz <pauloakello56@gmail.com>
2023-07-03 10:00:42 -0700

    updates to task 3

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# Checking if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-  echo "Error: No URL provided."
-  exit 1
-fi
-
-url="$1"
-
-# Send a DELETE request to the URL and store the response
-response=$(curl -s -X DELETE "$url")
-
-# Display the body of the response
-echo "$response"
-
+# script for sending a delete request
+curl -sX DELETE "$1"
