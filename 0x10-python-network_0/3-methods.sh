61bcb26f24f4131de491ab23f6f39bb5891e9d9d
blackoaz <pauloakello56@gmail.com>
2023-07-03 10:15:51 -0700

    adding task 5 for sending a get request

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# script for displaying allowed options in a http request
-curl -sI "$1" | grep 'Allow' | cut -d "" -f 2-
+# script for displaying allowed methods in a http request
+#!/bin/bash
+curl -sI "$1" | grep 'Allow' | cut -d " " -f 2-
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script for sending a GET request to the URL
+curl -sX GET -H "X-School-User-Id: 98" "$1"
