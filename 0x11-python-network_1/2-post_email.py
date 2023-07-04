c26ed35e1deaedf44db824cdf9b85e12b1052c0f
blackoaz <pauloakello56@gmail.com>
2023-07-04 12:54:00 -0700

    ádding task 3

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A script that takes in a URL as arg 1 to send a POST request with email
+   as the value argument 2
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
