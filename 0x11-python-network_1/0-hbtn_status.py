4c37412758ec75174fbdf8ba55c70d4109033c61
blackoaz <pauloakello56@gmail.com>
2023-07-04 11:39:31 -0700

    correcting a pycodestyle for long line in line 13

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,5 @@ url = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     body = response.read().decode('utf-8')
 
-print(f"Body response:\n\t- type: {type(body)}\n\t- content: {body}\n\t- utf8 content: {body}")
+print(f"Body response:\n\t- type: {type(body)}\n\t- content: {body}\n\t- utf8\
+        content: {body}")
